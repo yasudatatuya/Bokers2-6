f9a3bd3a501a29a2194992b1d50224f8c1a8aaa2
EC2 Default User <ec2-user@ip-172-31-20-175.ec2.internal>
2020-11-08 07:40:24 +0000

    start

---
+++ app/models/book.rb
@@ -0,0 +1,28 @@
+class Book < ApplicationRecord
+
+     belongs_to :user
+     has_many :book_comments, dependent: :destroy
+     has_many :favorites, dependent: :destroy
+
+     def favorited_by?(user)
+          favorites.where(user_id: user.id).exists?
+     end
+
+
+    validates :title, presence: true
+    validates :body, presence: true, length: { maximum: 200 }
+
+     def self.search(search,word)
+          if search == "forward_match"
+               @book = Book.where("title LIKE?","#{word}%")
+          elsif search == "backward_match"
+               @book = Book.where("title LIKE?","%#{word}")
+          elsif search == "perfect_match"
+               @book = Book.where("title LIKE?","#{word}")
+          elsif search == "partial_match"
+               @book = Book.where("title LIKE?","%#{word}%")
+          else
+               @book = Book.all
+          end
+     end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/about'
+  #get 'search' => 'searches#search'
+  get 'search/search'
+  root to:'home#top'
+  resources :users, only: [:show, :index, :edit, :update] do
+    member do
+      get :following, :followers
+      end
+    end
+  resources :relationships, only: [:create, :destroy]
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
